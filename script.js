c691a284b01447a135aa904c97f91eefc5a7114f
snegha032002 <sneghakumar0315@gmail.com>
2024-12-30 16:33:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	 const charCount = {};
+	for (let char of str) {
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
+	 for (let char of str) {
+    if (charCount[char] === 1) {
+      return char;
+    }
+  }
+ return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
